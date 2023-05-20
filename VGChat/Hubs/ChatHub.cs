f3565aa8e9c01b78a0911efaf130b00228ab110a
Said Berk <csaidberk@gmail.com>
2023-05-20 23:45:35 +0300

    SQL Database connections are done. Messages have sent being saved to database and shown at index page using signalR.

--- VGChat/Hubs/ChatHub.cs
+++ VGChat/Hubs/ChatHub.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using VGChat_demo.Data;
+using VGChat_demo.Models;
 
-namespace VGChat.Hubs
+namespace VGChat_demo.Hubs { 
+
+
+public class ChatHub : Hub
 {
-    public class ChatHub:Hub
-    { 
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
+    private readonly ApplicationDbContext _dbContext;
+
+    public ChatHub(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
     }
+
+    public async Task SendMessage(string user, string content)
+    {
+        var message = new Message
+        {
+            userName = user,
+            Content = content,
+            MessageSentTime = DateTime.UtcNow
+        };
+
+        _dbContext.Messages.Add(message);
+        await Clients.All.SendAsync("ReceiveMessage", message.userName, message.Content, message.MessageSentTime);
+        await _dbContext.SaveChangesAsync();
+
+       
+    }
+}
 }
--- VGChat/Models/ErrorViewModel.cs
+++ VGChat/Models/ErrorViewModel.cs
@@ -1,9 +1,9 @@
-namespace VGChat.Models
+namespace VGChat_demo.Models
 {
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
